cb1e0e3e371a410121532116021e6bcee4dccc97
aseprite1 <dmd5760@gmail.com>
2022-02-24 00:13:42 +0900

    4th commit

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -136,21 +136,22 @@ function getData(url) {
 function newsFeed() {
   var newsFeed = getData(NEWS_URL);
   var newsList = [];
-  newsList.push('<ul>');
+  var template = "\n    <div class=\"container mx-auto p-4\">\n      <h1>Hacker News</h1>\n      <ul>\n        {{__news_feed__}}      \n      </ul>\n      <div>\n        <a href=\"#/page/{{__prev_page__}}\">\uC774\uC804 \uD398\uC774\uC9C0</a>\n        <a href=\"#/page/{{__next_page__}}\">\uB2E4\uC74C \uD398\uC774\uC9C0</a>\n      </div>\n    </div>\n  ";
 
   for (var i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
-    newsList.push("\n            <li>\n             <a href=\"#/show/".concat(newsFeed[i].id, "\">\n                ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n             </a>\n            </li>\n        "));
+    newsList.push("\n      <li>\n        <a href=\"#/show/".concat(newsFeed[i].id, "\">\n          ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n        </a>\n      </li>\n    "));
   }
 
-  newsList.push('</ul>');
-  newsList.push("\n     <div>\n         <a href=\"#/page/".concat(store.currentPage > 1 ? store.currentPage - 1 : 1, "\">\uC774\uC804 \uD398\uC774\uC9C0</a>\n         <a href=\"#/page/").concat(store.currentPage + 1, "\">\uB2E4\uC74C \uD398\uC774\uC9C0</a>\n     </div>\n    "));
-  container.innerHTML = newsList.join('');
+  template = template.replace('{{__news_feed__}}', newsList.join(''));
+  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
+  template = template.replace('{{__next_page__}}', store.currentPage + 1);
+  container.innerHTML = template;
 }
 
 function newsDetail() {
   var id = location.hash.substr(7);
   var newsContent = getData(CONTENT_URL.replace('@id', id));
-  container.innerHTML = "\n    <hl>".concat(newsContent.title, "</hl>  \n        <div>\n            <a href=\"#/page/").concat(store.currentPage, "\"> \uBAA9\uB85D\uC73C\uB85C </a>\n        </div>\n    ");
+  container.innerHTML = "\n    <h1>".concat(newsContent.title, "</h1>\n\n    <div>\n      <a href=\"#/page/").concat(store.currentPage, "\">\uBAA9\uB85D\uC73C\uB85C</a>\n    </div>\n  ");
 }
 
 function router() {
@@ -196,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11136" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11712" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
